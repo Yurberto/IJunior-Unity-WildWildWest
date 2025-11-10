2129730d39313b5ea8bee23d04fe3d2ed8ec0cf8
Yurberto <sta1993lone@gmail.com>
2025-11-10 19:27:32 +0300

    fixed Camera

--- Assets/Sources/Scripts/Gameplay/Camera/Implementations/ThirdPersonCameraService.cs
+++ Assets/Sources/Scripts/Gameplay/Camera/Implementations/ThirdPersonCameraService.cs
@@ -35,11 +35,15 @@ public class ThirdPersonCameraService
         _mouseY = Mathf.Clamp(_mouseY, _cameraSetting.MinYAngle, _cameraSetting.MaxYAngle);
 
         Quaternion rotation = Quaternion.Euler(_mouseY, _mouseX, 0);
-        Vector3 offset = rotation * new Vector3(_cameraSetting.OffsetX, 0, -_cameraSetting.Distance);
-        Vector3 desiredPosition = _target.position + Vector3.up * _cameraSetting.Height + offset;
+        
+        Vector3 distanceOffset = rotation * new Vector3(0, 0, -_cameraSetting.Distance);
+        Vector3 verticalOffset = Vector3.up * _cameraSetting.Height;
+        Vector3 horizontalOffset = _camera.right * _cameraSetting.OffsetX;
+
+        Vector3 desiredPosition = _target.position + horizontalOffset + verticalOffset + distanceOffset;
 
         _camera.position = desiredPosition;
-        _camera.LookAt(_target.position + Vector3.up * _cameraSetting.Height);
+        _camera.LookAt(_target.position + Vector3.up * _cameraSetting.Height + _camera.right * _cameraSetting.OffsetX);
     }
 
     private void UpdateLook(Vector2 mouseDelta)
